f1a31cd4cfff518a4ddd0a93e2c076ae6c026dbf
LiChuan <973541075@qq.com>
2018-11-29 17:35:01 +0800

    webpack4零配置搭建基础项目

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import style from './main.css'
+
+class Test {
+  constructor () {
+    this.a = 'hello webpack!'
+  }
+}
+const arr = [1, 2, 3, 4]
+let obj = new Test()
+
+console.log(obj.a)
+console.log(arr)
